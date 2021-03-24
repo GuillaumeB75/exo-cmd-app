fe3047001150752858d1346199f5f7288b5850d1
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-03-24 10:53:50 +0100

    correction Sofiane live

--- pyramid.js
+++ pyramid.js
@@ -4,6 +4,15 @@ const pyramid = (nbStars, str) => {
   }
 }
 
+if (process.argv.length !== 4) {
+  console.log('usage : node pyramid.js nbStars str')
+  process.exit(1)
+}
+if (isNaN(process.argv[2])) {
+  console.log(`error : ${process.argv[2]} is not a number`)
+  process.exit(1)
+}
+  
 let nbStars = process.argv[2]
 let str = process.argv[3]
 pyramid(nbStars,str)
